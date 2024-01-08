27a0fb7527806349273ac9bef162a5dc9ca8381b
williamguilhermesouza <williamguilhermesouza@gmail.com>
2024-01-08 15:13:31 -0300

    instaling packagens and working on structure

---
+++ OrderAccumulator/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OrderAccumulator.Models;
+
+namespace OrderAccumulator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<OrderModel>> GetAllOrders()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ OrderAccumulator/Models/OrderModel.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderAccumulator.Models
+{
+    public class OrderModel
+    {
+        public int Id {  get; set; }
+        public required string Name { get; set; }
+        public required string Side { get; set; }
+        public required int Quantity { get; set; }
+        public required decimal Price { get; set; }
+    }
+}
